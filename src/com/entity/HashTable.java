3db247414ada28f85271ed11812374dab2bde82e
Madu Mahesh Kumar <rakeshmadu24@gmail.com>
2022-08-13 15:06:34 +0530

    uc1

---
+++ src/com/entity/HashTable.java
@@ -0,0 +1,73 @@
+package com.entity;
+
+public class HashTable {
+	
+	private static class ListNode {
+		int count;
+		String key;
+		ListNode next;
+		
+		public ListNode(String key) {
+			// TODO Auto-generated constructor stub
+			this.key = key;
+			this.count = 1;
+		}
+	}
+	
+	ListNode table[];
+	
+	public HashTable(int size) {
+		// TODO Auto-generated constructor stub
+		table = new ListNode[size];
+	}
+	
+	public int hash(String key) {
+		return Math.abs(key.hashCode()) % table.length;
+	}
+	
+	public void insert(String key, int bucket) {
+		table[bucket] = new ListNode(key);
+	}
+	
+	public void put(String key) {
+		int bucket = hash(key);
+		
+		if (table[bucket] == null) {
+			ListNode newNode = new ListNode(key);
+			table[bucket] = newNode;			
+		}
+		else {
+			while(!table[bucket].key.equals(key)) {
+				System.out.println("\n Searching for an empty or equal position for " + key);
+				bucket = ++bucket % table.length;
+				if (table[bucket] == null) {
+					System.out.println(" Found an empty position at " + bucket);
+					table[bucket] = new ListNode(key);
+					return;
+				}
+			}
+			System.out.println(" Found the word and incrementing count ");
+			table[bucket].count++;
+			
+		}
+	}
+	
+	public void display() {
+		System.out.println(" \n ----Display----");
+		for (ListNode currentNode : table) {
+			if(currentNode == null)
+				System.out.println(" null");
+			else
+				System.out.println(currentNode.key + ": " + currentNode.count);
+		}
+	}
+	
+	public void countWords(String str) {
+		for( String word : str.split(" ")) {
+			System.out.println(" word: " + word);
+			put(word.toLowerCase());
+		}
+		display();
+	}
+}
+	
---
+++ src/com/entity/HashTest.java
@@ -0,0 +1,15 @@
+package com.entity;
+
+public class HashTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "To be or not to be";
+		
+		HashTable hash = new HashTable(6);
+		hash.countWords(str);
+		
+	}
+
+}
